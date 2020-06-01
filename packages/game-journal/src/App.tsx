93ebde460758550c97db3dcc8ce23767783895fd
Carlos Castillo <castillo.st@gmail.com>
2020-06-01 08:46:43 +0200

    feat: add main content area

--- packages/game-journal/src/App.tsx
+++ packages/game-journal/src/App.tsx
@@ -1,23 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { MyNewComponent } from '@carloscasalar/companion-ui';
+import { CSSReset, ThemeProvider, Box } from '@chakra-ui/core';
+import { theme } from './theme/theme';
 
-function App() {
+const App = () => {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <MyNewComponent text="this is our library component" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <ThemeProvider theme={theme}>
+            <CSSReset />
+            <Box bg={'teal.700'} height="100vh" width="100vw">
+                content
+            </Box>
+        </ThemeProvider>
     );
-}
+};
 
 export default App;
