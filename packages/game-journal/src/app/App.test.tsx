126e0d513385e4f016e1bec4e878c7834e04df4f
Carlos Castillo <castillo.st@gmail.com>
2020-06-03 09:39:02 +0200

    feat: add a basic router

--- packages/game-journal/src/app/App.test.tsx
+++ packages/game-journal/src/app/App.test.tsx
@@ -4,25 +4,25 @@ import { mount, shallow, ShallowWrapper } from 'enzyme';
 import { Header } from './layout/Header';
 
 jest.mock('./layout/Header', () => ({
-    Header() {
-        return <div>the header</div>;
-    },
+  Header() {
+    return <div>the header</div>;
+  },
 }));
 
 describe('<App />', () => {
-    it('renders without crashing', () => {
-        const component = mount(<App />);
-        component.unmount();
+  it('renders without crashing', () => {
+    const component = mount(<App />);
+    component.unmount();
+  });
+
+  describe('when rendered', () => {
+    let component: ShallowWrapper;
+    beforeEach(() => {
+      component = shallow(<App />);
     });
 
-    describe('when rendered', () => {
-        let component: ShallowWrapper;
-        beforeEach(() => {
-            component = shallow(<App />);
-        });
-
-        it('should render the header', () => {
-            expect(component.find(Header).exists()).toBe(true);
-        });
+    it('should render the header', () => {
+      expect(component.find(Header).exists()).toBe(true);
     });
+  });
 });
--- packages/game-journal/src/app/App.tsx
+++ packages/game-journal/src/app/App.tsx
@@ -4,18 +4,23 @@ import { CSSReset, ThemeProvider, Box } from '@chakra-ui/core';
 import { theme } from './theme/theme';
 import { Header } from './layout/Header';
 import { rootStore } from './store/rootStore';
+import { BrowserRouter } from 'react-router-dom';
+import { Router } from './Router';
 
 const App = () => {
-    return (
-        <ThemeProvider theme={theme}>
-            <CSSReset />
-            <Provider store={rootStore}>
-                <Box bg={'teal.700'} height="100vh" width="100vw">
-                    <Header />
-                </Box>
-            </Provider>
-        </ThemeProvider>
-    );
+  return (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Provider store={rootStore}>
+        <BrowserRouter>
+          <Box bg={'teal.700'} height="100vh" width="100vw">
+            <Header />
+            <Router />
+          </Box>
+        </BrowserRouter>
+      </Provider>
+    </ThemeProvider>
+  );
 };
 
 export default App;
