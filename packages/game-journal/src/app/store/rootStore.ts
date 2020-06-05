3766a8b06c1c6c29b8b8f4565f5b611260291756
Carlos Castillo <castillo.st@gmail.com>
2020-06-05 21:20:44 +0200

    chore: Add composeWithDevTools to root store

--- packages/game-journal/src/app/store/rootStore.ts
+++ packages/game-journal/src/app/store/rootStore.ts
@@ -1,10 +1,11 @@
 import { StateType } from 'typesafe-actions';
 import { combineReducers, createStore } from 'redux';
 import { auth } from '../../features/auth/reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   auth,
 });
 export type RootState = StateType<typeof rootReducer>;
 
-export const rootStore = createStore(rootReducer);
+export const rootStore = createStore(rootReducer, composeWithDevTools());
