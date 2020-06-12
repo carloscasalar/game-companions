302c8cc4c17975ea5d5307f0a1fde4454c83142f
Carlos Castillo <castillo.st@gmail.com>
2020-06-12 07:48:31 +0200

    chore: Add types for netlify functions

---
+++ packages/game-journal-services/src/netlify/types.ts
@@ -0,0 +1,44 @@
+export interface Response<TBody> {
+  statusCode: number;
+  body: TBody;
+}
+export type Callback<TBody> = (
+  error: Error | null,
+  response: Response<TBody>
+) => void;
+
+export interface Context {
+  isBase64Encoded: boolean;
+  statusCode: number;
+  headers: { [header: string]: string };
+  body: string;
+}
+
+export interface Event {
+  path: string;
+  httpMethod:
+    | 'get'
+    | 'head'
+    | 'post'
+    | 'put'
+    | 'delete'
+    | 'connect'
+    | 'options'
+    | 'trace'
+    | 'patch';
+  headers: { [header: string]: string };
+  queryStringParameters: { [parameter: string]: string };
+  body: string;
+  isBase64Encoded: boolean;
+}
+
+export type HandlerWithCallback<TBody> = (
+  event: Event,
+  context: Context,
+  callback: Callback<TBody>
+) => void;
+
+export type Handler<TBody> = (
+  event: Event,
+  context: Context
+) => Promise<Response<TBody>>;
