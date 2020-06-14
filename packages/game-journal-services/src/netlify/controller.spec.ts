240b71f51431452dbfc7423385776aaec335a47a
Carlos Castillo <castillo.st@gmail.com>
2020-06-14 18:29:20 +0200

    chore: add script to launch unit test on lambda functions

--- packages/game-journal-services/src/netlify/controller.spec.ts
+++ packages/game-journal-services/src/netlify/controller.spec.ts
@@ -26,9 +26,9 @@ describe('netlify/controller', () => {
   describe('a controller with only GET handler', () => {
     let controllerWithGet: LambdaController;
     beforeEach(() => {
-      const getHandler: WithHttpMethodHandler<'GET', string> = {
+      const getHandler: WithHttpMethodHandler<'GET', { aText: string }> = {
         get() {
-          return Promise.resolve('ok response');
+          return Promise.resolve({ aText: 'a text' });
         },
       };
 
@@ -36,6 +36,38 @@ describe('netlify/controller', () => {
         ...controller,
         ...getHandler,
       };
+    });
+
+    describe('when handle a GET request', () => {
+      let response: LambdaResponse;
+      beforeEach(async () => {
+        const event: LambdaEvent = {
+          httpMethod: 'GET',
+          queryStringParameters: {},
+          isBase64Encoded: false,
+          headers: {},
+          path: 'a/path',
+          body: '',
+        };
+        const lambdaContext: LambdaContext = {
+          headers: {},
+          isBase64Encoded: true,
+          statusCode: 200,
+          body: '',
+        };
+        response = await controllerWithGet.handler(event, lambdaContext);
+      });
+
+      it('should respond OK with stringified body', () => {
+        expect(response).toEqual({
+          body: '{"aText":"a text"}',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          isBase64Encoded: false,
+          statusCode: 200,
+        });
+      });
     });
 
     describe.each`
